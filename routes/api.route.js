b852a01575f86143aa3d59672d3d6ba3453cf025
kidus Tesfaye <kidus.z3rd@gmail.com>
2023-09-24 21:54:33 +0300

    basic blog routs added and prisma realtion created

--- routes/api.route.js
+++ routes/api.route.js
@@ -1,7 +1,28 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-router.get('/', async (req, res, next) => {
-  res.send({ message: 'Ok api is working 🚀' });
+router.get("/", async (req, res, next) => {
+  res.send({ message: "Ok api is working 🚀" });
+});
+
+//working with blog
+
+router.get("/blogs", async (req, res, next) => {
+  res.send({ message: "blog get all page " });
+});
+
+router.get("/blogs/:id", async (req, res, next) => {
+  res.send({ message: "blog get one page " });
+});
+router.post("/blogs", async (req, res, next) => {
+  res.send({ message: "blog post page" });
+});
+
+router.patch("/blogs/:id", async (req, res, next) => {
+  res.send({ message: "blog patch page " });
+});
+
+router.delete("/blogs/:id", async (req, res, next) => {
+  res.send({ message: "blog delete page " });
 });
 
 module.exports = router;
