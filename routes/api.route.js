bc2017d467da841e81f5570dc7ce43199fc82d42
kidus Tesfaye <kidus.z3rd@gmail.com>
2023-09-26 12:33:24 +0300

    blog get by id from api made

--- routes/api.route.js
+++ routes/api.route.js
@@ -15,39 +15,4 @@ router.get("/", async (req, res, next) => {
 
 //working with blog
 
-//Get all blog
-router.get("/blogs", async (req, res, next) => {
-  try {
-    const blogs = await prisma.blog.findMany({});
-    res.json(blogs);
-  } catch (error) {
-    next(error);
-  }
-});
-
-//Get one blog
-router.get("/blogs/:id", async (req, res, next) => {
-  res.send({ message: "blog get one page " });
-});
-
-//Create blog
-router.post("/blogs", async (req, res, next) => {
-  try {
-    const blog = await prisma.blog.create({
-      data: req.body,
-    });
-    res.json(blog);
-  } catch (error) {}
-});
-
-//Update blog
-router.patch("/blogs/:id", async (req, res, next) => {
-  res.send({ message: "blog patch page " });
-});
-
-//delete Blog
-router.delete("/blogs/:id", async (req, res, next) => {
-  res.send({ message: "blog delete page " });
-});
-
 module.exports = router;
---
+++ routes/blogs.route.js
@@ -0,0 +1,53 @@
+const router = require("express").Router();
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
+//working with blog
+
+//Get all blog
+router.get("/", async (req, res, next) => {
+  try {
+    const blogs = await prisma.blog.findMany({});
+    res.json(blogs);
+  } catch (error) {
+    next(error);
+  }
+});
+
+//Get one blog
+router.get("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const singleBlog = await prisma.blog.findUnique({
+      where: {
+        id: id,
+      },
+    });
+    res.json(singleBlog);
+  } catch (error) {
+    next(error);
+  }
+});
+
+//Create blog
+router.post("/", async (req, res, next) => {
+  try {
+    const blog = await prisma.blog.create({
+      data: req.body,
+    });
+    res.json(blog);
+  } catch (error) {}
+});
+
+//Update blog
+router.patch("/:id", async (req, res, next) => {
+  res.send({ message: "blog patch page " });
+});
+
+//delete Blog
+router.delete("/:id", async (req, res, next) => {
+  res.send({ message: "blog delete page " });
+});
+
+module.exports = router;
